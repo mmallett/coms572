be3f6c0f37cb33ee40ead19fa146b47bb8afe79e
mmallett <mmallett91@gmail.com>
2013-09-27 17:09:17 -0500

    define strategy interface

--- lab1/src/edu/iastate/coms572/lab1/Strategy.java
+++ lab1/src/edu/iastate/coms572/lab1/Strategy.java
@@ -1,5 +1,33 @@
 package edu.iastate.coms572.lab1;
 
-public interface Strategy {
+import java.util.List;
+
+public abstract class Strategy {
+	
+	protected final StrategyType type;
+	
+	public final StrategyType getStrategyType(){
+		return type;
+	}
+	
+	public enum StrategyType{
+		
+		BFS("breadth"),
+		DFS("depth"),
+		BEST("best"),
+		BEAM("beam");
+		
+		private final String value;
+
+		StrategyType(String value){
+			this.value = value;
+		}	
+	}
+	
+	public Strategy(StrategyType type){
+		this.type = type;
+	}
+	
+	public abstract SearchNode getNextNode(SearchNode currentNode, List<SearchNode> fringe);
 
 }
