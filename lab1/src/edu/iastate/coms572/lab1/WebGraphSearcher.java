2c542dd488522e598b8185be56645e15c9a5c096
Mathew Mallett <mmallett@iastate.edu>
2013-09-29 20:50:12 -0500

    link parser class

--- lab1/src/edu/iastate/coms572/lab1/WebGraphSearcher.java
+++ lab1/src/edu/iastate/coms572/lab1/WebGraphSearcher.java
@@ -1,23 +1,14 @@
 package edu.iastate.coms572.lab1;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import edu.iastate.coms572.lab1.util.FileLoader;
 
 public abstract class WebGraphSearcher {
-	
-	protected Set<String> openSet;
-	
-	protected Set<String> closedSet;
 	
 	protected String startNode;
 	
 	protected  String goalPattern;
 	
 	public WebGraphSearcher(String startNode, String goalPattern, FileLoader loader){
-		this.openSet = new HashSet<String>();
-		this.closedSet = new HashSet<String>();
 		this.startNode = startNode;
 		this.goalPattern = goalPattern;
 		
